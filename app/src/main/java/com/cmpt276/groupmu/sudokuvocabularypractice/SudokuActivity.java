eed0037085e97d78963903eedf33e5536527bd70
Alan Deng <hda25@sfu.ca>
2019-02-01 16:44:30 +0800

    GridView layout Settings

--- app/src/main/java/com/cmpt276/groupmu/sudokuvocabularypractice/SudokuActivity.java
+++ app/src/main/java/com/cmpt276/groupmu/sudokuvocabularypractice/SudokuActivity.java
@@ -1,13 +1,40 @@
 package com.cmpt276.groupmu.sudokuvocabularypractice;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SudokuActivity extends AppCompatActivity {
-
+    private TextView gridtext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sudoku);
+         final int[] puzzle = {
+                1, 2, 3, 4, 5, 6, 7, 8, 9,
+                4, 5, 6, 7, 8, 9, 1, 2, 3,
+                7, 8, 9, 1, 2, 3, 4, 5, 6,
+                2, 3, 4, 5, 6, 7, 8, 9, 1,
+                5, 6, 7, 8, 9, 1, 2, 3, 4,
+                8, 9, 1, 2, 3, 4, 5, 6, 7,
+                3, 4, 5, 0, 0, 0, 9, 1, 2,
+                6, 0, 0, 0, 0, 0, 0, 0, 0,
+                9, 0, 0, 0, 0, 0, 0, 0, 0
+        };
+
+        GridView grid = findViewById(R.id.grid);
+        //TextView gridtext = findViewById(R.id.gridText);
+        grid.setAdapter(new SudokuAdapter(this, puzzle));
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                //Toast.makeText(this, "" , Toast.LENGTH_SHORT).show();
+    }
+
+    });
     }
 }
--- app/src/main/java/com/cmpt276/groupmu/sudokuvocabularypractice/SudokuAdapter.java
+++ app/src/main/java/com/cmpt276/groupmu/sudokuvocabularypractice/SudokuAdapter.java
@@ -1,4 +1,59 @@
 package com.cmpt276.groupmu.sudokuvocabularypractice;
 
-public class SudokuAdapter {
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+
+public class SudokuAdapter extends BaseAdapter {
+
+    private Context context;
+    private int[] textViewVals;
+    ;
+    public SudokuAdapter(Context c, int[] textViewVals) {
+        this.context = c;
+        this.textViewVals = textViewVals;
+
+
+    }
+    public int getCount() {
+        return textViewVals.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //LayoutInflater inflater = (LayoutInflater) context         .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        TextView textView;
+        if (convertView == null) {
+
+            textView = new TextView(context);
+
+            //textView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));//inflater.inflate(R.layout.item, null);
+            textView.setBackgroundColor(Color.GRAY);
+            textView.setLayoutParams(new GridView.LayoutParams(GridView.AUTO_FIT, 125));
+            textView.setText(textViewVals[position]+"");
+
+            // set value into textview
+//            TextView textView = (TextView) gridView
+//                    .findViewById(R.id.the_grid_label);
+//            textView.setText(textViewVals[position]);
+        } else {
+            textView = (TextView) convertView;
+        }
+
+        return textView;
+    }
 }
