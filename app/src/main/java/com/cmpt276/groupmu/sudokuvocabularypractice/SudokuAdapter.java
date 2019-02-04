8d4a1a6ed1bc99219de495a382f99ec326d01def
Alan Deng <hda25@sfu.ca>
2019-02-04 14:16:52 +0800

    Initial sudoku  game ( 3x3 region check method is not yet completed)

--- app/src/main/java/com/cmpt276/groupmu/sudokuvocabularypractice/SudokuAdapter.java
+++ app/src/main/java/com/cmpt276/groupmu/sudokuvocabularypractice/SudokuAdapter.java
@@ -3,7 +3,6 @@ package com.cmpt276.groupmu.sudokuvocabularypractice;
 import android.content.Context;
 import android.graphics.Color;
 import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -13,20 +12,19 @@ import android.widget.TextView;
 public class SudokuAdapter extends BaseAdapter {
 
     private Context context;
-    private int[] textViewVals;
-    ;
-    public SudokuAdapter(Context c, int[] textViewVals) {
+    private String[] textViewVals;
+
+    public SudokuAdapter(Context c, String[] textViewVals) {
         this.context = c;
         this.textViewVals = textViewVals;
-
-
     }
+
     public int getCount() {
         return textViewVals.length;
     }
 
     public Object getItem(int position) {
-        return null;
+        return textViewVals[position];
     }
 
     public long getItemId(int position) {
@@ -42,9 +40,9 @@ public class SudokuAdapter extends BaseAdapter {
             textView = new TextView(context);
 
             //textView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));//inflater.inflate(R.layout.item, null);
-            textView.setBackgroundColor(Color.GRAY);
-            textView.setLayoutParams(new GridView.LayoutParams(GridView.AUTO_FIT, 125));
-            textView.setText(textViewVals[position]+"");
+            textView.setBackgroundColor(Color.LTGRAY);
+            textView.setLayoutParams(new GridView.LayoutParams(GridView.AUTO_FIT, 122));
+            textView.setText(textViewVals[position].toString());
 
             // set value into textview
 //            TextView textView = (TextView) gridView
